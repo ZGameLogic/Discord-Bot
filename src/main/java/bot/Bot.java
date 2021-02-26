1e064b3b9095d97cc82efbc97a2bb16df6cb0595
Ben Shabowski <benshabowski@gmail.com>
2021-02-26 11:01:23 -0600

    Adding comments for read-ability

--- src/main/java/bot/Bot.java
+++ src/main/java/bot/Bot.java
@@ -23,11 +23,6 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class Bot {
 
-	/*
-	 * Create chat IDS
-	 */
-	private LinkedList<Long> createChatIDs;
-
 	// bot
 	private JDA bot;
 
@@ -36,8 +31,15 @@ public class Bot {
 
 	// voice channel category
 	private Category chatRoomsCat;
-	LinkedList<Long> ignoredChannelIDs;
-	LinkedList<Long> textChannelIDS;
+
+	//  IDs for voice channels that create chat rooms
+	private LinkedList<Long> createChatIDs;
+	
+	// IDs for voice channels that get ignored for the deletion process
+	private LinkedList<Long> ignoredChannelIDs;
+	
+	// IDs for text channels that can be sent commands too
+	private LinkedList<Long> textChannelIDS;
 
 	public Bot() {
 		
@@ -53,22 +55,28 @@ public class Bot {
 			e1.printStackTrace();
 		}
 		
-
 		createChatIDs = new LinkedList<Long>();
+		// Shlongshot chat ID
 		createChatIDs.add(812095961475317811l);
+		// Test server chat ID
 		createChatIDs.add(812083428686168136l);
 
 		ignoredChannelIDs = new LinkedList<Long>();
+		// Ignore all create chat room IDs
 		ignoredChannelIDs.addAll(createChatIDs);
+		// AFK channel ID for Shlongshot
 		ignoredChannelIDs.add(371695546173358090l);
 
 		textChannelIDS = new LinkedList<Long>();
+		// Shlongshot text ID
 		textChannelIDS.add(812909596175106048l);
+		// Test server text ID
 		textChannelIDS.add(812912306357928007l);
 
 		JDABuilder bot = JDABuilder.createDefault(token);
 		bot.addEventListeners(new Listener());
 
+		// Login
 		try {
 			this.bot = bot.build().awaitReady();
 		} catch (LoginException | InterruptedException e) {
@@ -83,6 +91,7 @@ public class Bot {
 			}
 		}
 
+		// Get the category (This is for determining if the chat room should be deleted when done)
 		for (Category x : this.bot.getCategories()) {
 			if (x.getName().contains("Chat Rooms")) {
 				System.out.println("Found category");
@@ -91,94 +100,128 @@ public class Bot {
 			}
 		}
 
+		// start the message poster
 		new messagePoster().start();
 	}
 
+	
+	/**
+	 * Listener classes for all the events 
+	 * @author Ben Shabowski
+	 *
+	 */
 	private class Listener extends ListenerAdapter {
 
+		/**
+		 * Login event
+		 */
 		@Override
 		public void onReady(ReadyEvent event) {
 			System.out.println("Logged in");
 		}
 
-		@SuppressWarnings("finally")
+		/**
+		 * When we get a message
+		 */
 		@Override
 		public void onMessageReceived(MessageReceivedEvent event) {
 			if (!event.getAuthor().isBot()) {
 				if (textChannelIDS.contains(event.getTextChannel().getIdLong())) {
-					// TODO add new command stuff in
+					
+					// boolean for valid command
 					boolean valid = false;
 					String command = event.getMessage().getContentDisplay();
 					
 					if(command.startsWith("rename ") && command.split(" ").length > 1) {
-						for(VoiceChannel x : shlongshot.getVoiceChannels()) {
-							if(x.getParent() == chatRoomsCat && x.getMembers().contains(event.getMember())) {
-								try {
-									x.getManager().setName(command.replace("rename ", "")).queue();
-									valid = true;
-								}catch(IllegalArgumentException e1) {
-									
-								}finally{
-									break;
-								}
+						// Command: rename <name>
+						// Renames channel to the name the user specifies
+						
+						// get the voice channel
+						VoiceChannel channel = event.getMember().getVoiceState().getChannel();
+						if(channel != null) {
+							try {
+								// rename the channel and set valid command to true
+								channel.getManager().setName(command.replace("rename ", "")).queue();
+								valid = true;
+							}catch(IllegalArgumentException e1) {
+								
 							}
 						}
 						
 					}else if(command.startsWith("limit ") && command.split(" ").length > 1) {
-						
+						// Command: limit <user limit number>
+						// puts a limit to the max amount of users that can join the channel
+
+						VoiceChannel channel = event.getMember().getVoiceState().getChannel();
 						try {
-							for(VoiceChannel x : shlongshot.getVoiceChannels()) {
-								if(x.getParent() == chatRoomsCat && x.getMembers().contains(event.getMember())) {
-									x.getManager().setUserLimit(Integer.parseInt(command.replace("limit ", ""))).queue();
-									break;
-								}
+							if (channel != null) {
+								// set the user limit to the channel
+								channel.getManager().setUserLimit(Integer.parseInt(command.replace("limit ", "")))
+										.queue();
+								valid = true;
 							}
-							
-							Integer.parseInt(command.split(" ")[1]);
-							valid = true;
-						}catch(NumberFormatException e) {
-							
-						}catch(IllegalArgumentException e1) {
-							
+						} catch (NumberFormatException e) {
+
+						} catch (IllegalArgumentException e1) {
+
 						}
 					}					
 					
 					if (!valid) {
+						// we get here if the command isnt valid. Lets send the dumb butt a message
 						event.getAuthor().openPrivateChannel().complete()
 						.sendMessage("Unknown or invalid command:" + event.getMessage().getContentDisplay())
 						.queue();
 					}
 
+					// delete the message from the channel
 					event.getMessage().delete().queue();
 				}
 			}
 		}
 
+		/**
+		 * On voice chat join
+		 */
 		@Override
 		public void onGuildVoiceJoin(GuildVoiceJoinEvent event) {
 			playerJoined(event.getChannelJoined(), event.getMember());
 		}
 
+		/**
+		 * On voice chat move
+		 */
 		@Override
 		public void onGuildVoiceMove(GuildVoiceMoveEvent event) {
 			playerLeft(event.getChannelLeft());
 			playerJoined(event.getChannelJoined(), event.getMember());
 		}
-
+		
+		/**
+		 * On voice chat leave
+		 */
 		@Override
 		public void onGuildVoiceLeave(GuildVoiceLeaveEvent event) {
 			playerLeft(event.getChannelLeft());
 		}
 
+		/**
+		 * Deletes this channel if there are no players in it and it matches the chat room parent
+		 * @param leftChannel The channel the player left
+		 */
 		private void playerLeft(VoiceChannel leftChannel) {
 			if (!ignoredChannelIDs.contains(leftChannel.getIdLong()) && leftChannel.getParent() != null
 					&& leftChannel.getParent().equals(chatRoomsCat) && leftChannel.getMembers().size() <= 0) {
 				// delete the channel
 				leftChannel.delete().queue();
-				// TODO rename regular Chatrooms
 			}
 		}
 
+		/**
+		 * If a user joins the create channel, create a room and move them to it
+		 * @param joinChannel The channel the player joined
+		 * @param user The player
+		 */
 		private void playerJoined(VoiceChannel joinChannel, Member user) {
 			// joined create channel
 			if (createChatIDs.contains(joinChannel.getIdLong())) {
@@ -190,31 +233,27 @@ public class Bot {
 				shlongshot.moveVoiceMember(user, newChannel).queue();
 			}
 		}
-		/*
-		
-		public void onSlashCommand(SlashCommandEvent event) {
-			  if (event.getCommandName().equals("async")) {
-			    event.acknowledge(false) // log the original message, respond later (because you only have 3 seconds to respond)
-			      .delay(10, TimeUnit.MINUTES)
-			      .flatMap(commandThread -> command.sendMessage("asynchronous response")) // this can be done up to 15 minutes later
-			      .queue();
-			  } else if (event.getCommandName().equals("ping")) {
-			    event.reply("Pong!").setEphemeral(true).queue();
-			  }
-			}
-		*/
 	}
 	
+	/**
+	 * This class handles posting a message in the bot channel. Will be removed once slash commands come out. 
+	 * @author Ben Shabowski
+	 *
+	 */
 	private class messagePoster extends Thread {
 		
 		public void run() {
 			while(true) {
-				
+				// message file
 				File input = new File("message.txt");
 				
 				if(input.exists()) {
+					// we get here if the file exists
+					
+					// text channel to send the message too
 					TextChannel messageChannel = null;
 					
+					// get the channel by ID
 					for(Long x : textChannelIDS) {
 						if((messageChannel = shlongshot.getTextChannelById(x)) != null) {
 							break;
@@ -223,6 +262,7 @@ public class Bot {
 					
 					Scanner fileInput;
 					try {
+						// read the file, and then send out each line to the text channel
 						fileInput = new Scanner(input);
 						while(fileInput.hasNextLine()) {
 							messageChannel.sendMessage(fileInput.nextLine()).queue();
@@ -244,7 +284,5 @@ public class Bot {
 				}
 			}
 		}
-		
 	}
-
 }
