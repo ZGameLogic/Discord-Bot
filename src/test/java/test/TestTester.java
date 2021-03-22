30f6bd5f14640db2e4c7335fdd3a46e2c25e5be4
Ben Shabowski <benshabowski@gmail.com>
2021-03-22 09:45:58 -0500

    updating jar to not include config

--- src/test/java/test/TestTester.java
+++ src/test/java/test/TestTester.java
@@ -1,25 +1,9 @@
 package test;
 import static org.junit.Assert.*;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import data.ConfigLoader;
 
 public class TestTester {
-	
-	private static ConfigLoader config;
-	
-	@BeforeClass
-	public static void createConfig() {
-		// Load config
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-		context.scan("data");
-		context.refresh();
-		config = context.getBean(ConfigLoader.class);
-		context.close();
-	}
 
 	@Test
 	public void test() {
