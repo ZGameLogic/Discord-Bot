0f0a9f6db5d8532c729252105ecdbd0fa8873c7f
Ben Shabowski <benshabowski@gmail.com>
2021-04-02 11:36:53 -0500

    testing

--- src/main/java/webhook/listeners/WebHookListener.java
+++ src/main/java/webhook/listeners/WebHookListener.java
@@ -9,7 +9,6 @@ public class WebHookListener {
 	private static TextChannel channel;
 	
 	public WebHookListener(ConfigLoader cl, JDA bot) {
-		
 		channel = bot.getGuildById(cl.getBitbucketGuildIDs().get(0)).getTextChannelById(cl.getBitbucketGuildIDs().get(1));
 	}
 	
