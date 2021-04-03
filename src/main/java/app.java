78ce245f983fa947f5e2b11c412aae3d93c513da
Ben Shabowski <benshabowski@gmail.com>
2021-04-03 10:38:05 -0500

    SHLON-12 #done Hopefully fixing the logging

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -2,11 +2,12 @@ import java.util.Collections;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.PropertySource;
 
 import bot.Bot;
 
 @SpringBootApplication(scanBasePackages = {"webhook"})
+@PropertySource("application.properties")
 public class app {
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(app.class);
