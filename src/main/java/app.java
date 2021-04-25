4141418ffe2d1069b81d250d1b39fa19d43852b2
Ben Shabowski <benshabowski@gmail.com>
2021-04-25 14:00:07 -0500

    oops

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -2,12 +2,10 @@ import java.util.Properties;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 import bot.Bot;
 
 @SpringBootApplication(scanBasePackages = {"webhook"})
-@PropertySource("application.properties")
 public class app {
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(app.class);
