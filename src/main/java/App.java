047092083d216782cc9f21b34142d08040d17b3c
Ben Shabowski <benshabowski@gmail.com>
2022-01-13 10:57:07 -0600

    Removing author for test

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,11 +7,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import bot.Bot;
 import data.ConfigLoader;
 
-/**
- * 
- * @author Ben Shabowski
- *
- */
+
 @SpringBootApplication(scanBasePackages = {"webhook"})
 public class App {
 	
