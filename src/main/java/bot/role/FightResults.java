cc867060d0abf94ccef5026dca37181d493c1e17
Ben Shabowski <benshabowski@gmail.com>
2022-01-29 11:23:33 -0600

    SHLON-13 Adding fight logging

--- src/main/java/bot/role/FightResults.java
+++ src/main/java/bot/role/FightResults.java
@@ -10,5 +10,59 @@ public class FightResults {
 	private boolean attackerWon;
 	private int attackerPoints;
 	private int defenderPoints;
+	private int strengthTotal, strengthRolled;
+	private int magicTotal, magicRolled;
+	private int agilityTotal, agilityRolled;
+	private int knowledgeTotal, knowledgeRolled;
+	private int staminaTotal, staminaRolled;
+	private int defenderPadding, boosterPadding, paddingMultiplier;
+	private int attackerStrength, defenderStrength;
+	private int attackerStamina, defenderStamina;
+	private int attackerMagic, defenderMagic;
+	private int attackerAgility, defenderAgility;
+	private int attackerKnowledge, defenderKnowledge;
+	
+	public String strengthString() {
+		return "Strength: " + (strengthRolled <= attackerStrength ? "won" : "lost") + "\n"
+				+ "\tAttacker: " + attackerStrength + "\tDefender: " + defenderStrength + "\n"
+				+ "\tTotal: " + strengthTotal + "\tRolled: " + strengthRolled;
+	}
+	
+	public String knowledgeString() {
+		return "Knowledge: " + (knowledgeRolled <= attackerKnowledge ? "won" : "lost") + "\n"
+				+ "\tAttacker: " + attackerKnowledge + "\tDefender: " + defenderKnowledge + "\n"
+				+ "\tTotal: " + knowledgeTotal + "\tRolled: " + knowledgeRolled;
+	}
+	
+	public String staminaString() {
+		return "Stamina: " + (staminaRolled <= attackerStamina ? "won" : "lost") + "\n"
+				+ "\tAttacker: " + attackerStamina + "\tDefender: " + defenderStamina + "\n"
+				+ "\tTotal: " + staminaTotal + "\tRolled: " + staminaRolled;
+	}
+	
+	public String agilityString() {
+		return "Agility: " + (agilityRolled <= attackerAgility ? "won" : "lost") + "\n"
+				+ "\tAttacker: " + attackerAgility + "\tDefender: " + defenderAgility + "\n"
+				+ "\tTotal: " + agilityTotal + "\tRolled: " + agilityRolled;
+	}
+	
+	public String magicString() {
+		return "Strength: " + (strengthRolled <= attackerStrength ? "won" : "lost") + "\n"
+				+ "\tAttacker: " + attackerMagic + "\tDefender: " + defenderMagic + "\n"
+				+ "\tTotal: " + magicTotal + "\tRolled: " + magicRolled;
+	}
+	
+	public String toString() {
+		return "Attacker win: " + attackerWon +"\n"
+				+ "Score: " + attackerPoints + " " + defenderPoints + "\n"
+				+ "Booster padding: " + boosterPadding + "\n"
+				+ "Padding Multiplier: x" + paddingMultiplier + "\n"
+				+ "Defender padding level: " + defenderPadding + "\n"
+				+ strengthString() + "\n"
+				+ staminaString() + "\n"
+				+ agilityString() + "\n"
+				+ knowledgeString() + "\n"
+				+ magicString() + "\n";
+	}
 
 }
