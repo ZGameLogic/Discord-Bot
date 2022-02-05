2000775807b4d37b56f819382eef72b230643598
Ben Shabowski <benshabowski@gmail.com>
2022-02-04 19:17:01 -0600

    SHLON-16 Adding review bot functionality

--- src/main/java/bot/role/RoleBotListener.java
+++ src/main/java/bot/role/RoleBotListener.java
@@ -41,7 +41,6 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.ChannelType;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed.Field;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.TextChannel;
---
+++ src/main/java/bot/steam/SteamListener.java
@@ -0,0 +1,36 @@
+package bot.steam;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import controllers.steam.SteamAPI;
+import net.dv8tion.jda.api.events.ReadyEvent;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class SteamListener extends ListenerAdapter {
+
+	private Logger logger = LoggerFactory.getLogger(SteamListener.class);
+	
+	@Override
+	public void onReady(ReadyEvent event) {
+		logger.info("Steam bot listener activated");
+	}
+
+	@Override
+	public void onMessageReceived(MessageReceivedEvent event) {
+		if(event.getMessage().getContentRaw().contains("https://store.steampowered.com/app/")) {
+			for(String word : event.getMessage().getContentRaw().split(" ")) {
+				if(word.contains("https://store.steampowered.com/app/")) {
+					String appID = word.replace("https://store.steampowered.com/app/", "").split("/")[0];
+					SteamAppData SAD = SteamAPI.appReviews(appID);
+					if(event.isFromGuild()) {
+						event.getTextChannel().sendMessageEmbeds(SAD.generateEmbeds()).queue();
+					} else {
+						event.getPrivateChannel().sendMessageEmbeds(SAD.generateEmbeds()).queue();
+					}
+				}
+			}
+		}
+	}
+}
