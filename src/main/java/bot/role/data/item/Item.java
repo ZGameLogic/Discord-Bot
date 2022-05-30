c72ee09088eb695de43c2671f9beb397db48cf26
BShabowski <benshabowski@gmail.com>
2022-05-30 18:08:47 -0500

    DB-3 Small change to item durability

--- src/main/java/bot/role/data/item/Item.java
+++ src/main/java/bot/role/data/item/Item.java
@@ -1,7 +1,6 @@
 package bot.role.data.item;
 
 import lombok.*;
-import net.minidev.json.annotate.JsonIgnore;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -22,12 +21,19 @@ public class Item {
         COMMON
     }
 
+    public enum Material {
+        WOOD,
+        STEEL,
+        IRON,
+        BONE
+    }
+
     private List<Modifier> modifiers;
     private String name;
     private String description;
     private Rarity rarity;
-    private int maxUses;
-    private int uses;
+    private int maxDurability;
+    private int durability;
 
     /**
      * Creates an item with one modifier
@@ -38,23 +44,27 @@ public class Item {
      * @param type type of the item
      * @param amount amount of the stat the item effects
      */
-    public Item (String name, String description, Rarity rarity, Modifier.Stat stat, Modifier.Type type, int amount, int maxUses){
+    public Item (String name, String description, Rarity rarity, Modifier.Stat stat, Modifier.Type type, int amount, int maxDurability){
         Modifier modifier = new Modifier(stat, type, amount);
         modifiers = new LinkedList<>();
         modifiers.add(modifier);
         this.name = name;
         this.description = description;
         this.rarity = rarity;
-        this.maxUses = maxUses;
-        uses = 0;
+        this.maxDurability = maxDurability;
+        durability = 0;
     }
 
     public void useItem(){
-        uses++;
+        durability--;
+    }
+
+    public void repair(){
+        durability = maxDurability;
     }
 
     public boolean broken(){
-        return uses <= maxUses;
+        return durability <= 0;
     }
 
 }
