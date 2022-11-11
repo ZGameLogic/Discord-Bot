2fe3984078e73539f799c33ee49a548e7d993c26
Ben Shabowski <benshabowski@gmail.com>
2022-11-11 09:18:40 -0600

    Everything done except updating messages

--- src/main/java/bot/listeners/PlannerBot.java
+++ src/main/java/bot/listeners/PlannerBot.java
@@ -1,15 +1,21 @@
 package bot.listeners;
 
+import bot.utils.EmbedMessageGenerator;
 import com.zgamelogic.AdvancedListenerAdapter;
 import data.database.planData.Plan;
 import data.database.planData.PlanRepository;
+import data.database.planData.User;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.IMentionable;
 import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.channel.concrete.PrivateChannel;
 import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import net.dv8tion.jda.api.interactions.components.buttons.ButtonInteraction;
 import net.dv8tion.jda.api.interactions.components.selections.EntitySelectInteraction;
 import net.dv8tion.jda.api.interactions.components.selections.EntitySelectMenu;
@@ -17,6 +23,8 @@ import net.dv8tion.jda.api.interactions.components.text.TextInput;
 import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
 import net.dv8tion.jda.api.interactions.modals.Modal;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
 
 public class PlannerBot extends AdvancedListenerAdapter {
 
@@ -57,7 +65,7 @@ public class PlannerBot extends AdvancedListenerAdapter {
         String title = event.getValue("title").getAsString();
         int count = Integer.parseInt(event.getValue("count").getAsString());
 
-        event.reply("Select people to invite").setActionRow(
+        event.reply("Select people to invite (Don't include yourself). Plan id:" + event.getIdLong()).setActionRow(
                         EntitySelectMenu.create("People", EntitySelectMenu.SelectTarget.USER)
                                 .setMinValues(1)
                                 .setMaxValues(10)
@@ -68,36 +76,48 @@ public class PlannerBot extends AdvancedListenerAdapter {
                     plan.setNotes(notes);
                     plan.setAuthorId(event.getUser().getIdLong());
                     plan.setCount(count);
+                    plan.setId(event.getIdLong());
+                    planRepository.save(plan);
                 });
     }
 
     @ButtonResponse(buttonId = "accept_event")
     private void acceptEvent(ButtonInteraction event){
-        // TODO clear buttons
+        // TODO clear buttons for that user
         // TODO update database
-        // TODO update event modal
+        // TODO send message to event organizer
+        // TODO update event embed
+        // TODO update private message embeds
     }
 
     @ButtonResponse(buttonId = "deny_event")
     private void denyEvent(ButtonInteraction event){
-        // TODO clear buttons
+        // TODO clear buttons for that user
         // TODO update database
-        // TODO update event modal
+        // TODO send message to event organizer
+        // TODO update event embed
+        // TODO update private message embeds
     }
 
     @EntitySelectionResponse(menuId = "People")
     private void planPeople(EntitySelectInteraction event){
-        event.getMessage().editMessageComponents().queue();
-        event.getMessage().editMessage("").queue();
-        EmbedBuilder eb = new EmbedBuilder();
-        eb.setTitle("Invites status");
-        String desc = "";
+        long planId = Long.parseLong(event.getMessage().getContentRaw().split(":")[1]);
+        event.getMessage().delete().queue();
+        HashMap<Long, User> invitees = new HashMap<>();
         for(Member m : event.getMentions().getMembers()){
-            m.getUser().openPrivateChannel().queue(pm -> {
-                // TODO send the user a message
-            });
-            desc += m.getEffectiveName() + ": waiting for reply\n";
+            invitees.put(m.getIdLong(), new User(m.getIdLong(), 0));
         }
-        event.replyEmbeds(eb.build()).queue();
+        Plan plan = planRepository.getOne(planId);
+        plan.setInvitees(invitees);
+        for(Member m : event.getMentions().getMembers()){
+            PrivateChannel pm = m.getUser().openPrivateChannel().complete();
+            Message message = pm.sendMessageEmbeds(EmbedMessageGenerator.singleInvite(plan, event.getGuild()))
+                    .addActionRow(Button.success("accept_event", "Accept"),
+                            Button.danger("deny_event", "Deny"))
+                    .complete();
+            plan.updateMessageIdForUser(m.getIdLong(), message.getIdLong());
+        }
+        planRepository.save(plan);
+        event.replyEmbeds(EmbedMessageGenerator.plan(plan, event.getGuild())).queue();
     }
 }
