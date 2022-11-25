51fe56841b639bebfdf7fdd80cc21f90b390f5c0
Ben Shabowski <benshabowski@gmail.com>
2022-11-25 15:56:29 -0600

    The start of enabling

--- src/main/java/interfaces/atlassian/BambooInterface.java
+++ src/main/java/interfaces/atlassian/BambooInterface.java
@@ -1,4 +1,23 @@
 package interfaces.atlassian;
 
 public abstract class BambooInterface {
+
+    /**
+     * Checks if the URL is valid
+     * @param url URL to check if valid
+     * @return true if the URL is valid
+     */
+    public static boolean checkURL(String url){
+        return true;
+    }
+
+    /**
+     * Checks if a personal Access token is valid
+     * @param PAT personal access token
+     * @param url url to check against
+     * @return true if PAT is valid
+     */
+    public static boolean checkPAT(String PAT, String url){
+        return true;
+    }
 }
