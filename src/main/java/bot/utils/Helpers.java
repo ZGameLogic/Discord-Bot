fbba5bb03432904ffbdf8372aacd26fea03f8b36
Ben Shabowski <shabowskibenjaminm@johndeere.com>
2023-01-04 14:48:26 -0600

    Adding more date combos

---
+++ src/main/java/bot/utils/Helpers.java
@@ -0,0 +1,41 @@
+package bot.utils;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+
+public abstract class Helpers {
+
+    public static Date stringToDate(String dateString){
+        dateString = dateString.toUpperCase().replace("TODAY ", "").replace("AT ", "");
+        boolean tomorrow = dateString.contains("TOMORROW ");
+        if(tomorrow) dateString = dateString.replace("TOMORROW ", "");
+        HashMap<String, Integer[]> patterns = new HashMap<>();
+        patterns.put("EEEE h:mma", new Integer[]{Calendar.DAY_OF_WEEK, Calendar.HOUR_OF_DAY, Calendar.MINUTE});
+        patterns.put("M/dd/yyyy h:mma", new Integer[]{Calendar.YEAR, Calendar.MONTH, Calendar.DAY_OF_MONTH, Calendar.HOUR_OF_DAY, Calendar.MINUTE});
+        patterns.put("M/dd h:mma", new Integer[]{Calendar.MONTH, Calendar.DAY_OF_MONTH, Calendar.HOUR_OF_DAY, Calendar.MINUTE});
+        patterns.put("h:mma", new Integer[]{Calendar.HOUR_OF_DAY, Calendar.MINUTE});
+        for(String pattern: patterns.keySet()){
+            SimpleDateFormat formatter = new SimpleDateFormat(pattern, Locale.ENGLISH);
+            Calendar date = Calendar.getInstance();
+            Calendar formatted = Calendar.getInstance();
+            try {
+                formatted.setTime(formatter.parse(dateString));
+                for(int field: patterns.get(pattern)){
+                    date.set(field, formatted.get(field));
+                }
+                if(tomorrow) date.add(Calendar.DAY_OF_MONTH, 1);
+                while(date.getTime().before(new Date())){
+                    date.add(Calendar.DAY_OF_MONTH, 7);
+                }
+                return date.getTime();
+            } catch(ParseException ignored){
+
+            }
+        }
+        return null;
+    }
+}
