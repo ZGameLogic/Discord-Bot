059608778c7c9985f1dd5210977599e1205b6eed
BShabowski <benshabowski@gmail.com>
2023-01-16 13:56:17 -0600

    Didn't commit this, oops

---
+++ src/main/java/bot/listeners/WordleBot.java
@@ -0,0 +1,25 @@
+package bot.listeners;
+
+import com.zgamelogic.AdvancedListenerAdapter;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+public class WordleBot extends AdvancedListenerAdapter {
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if(event.getMessage().getContentRaw().startsWith("Wordle ")){
+            try {
+                String line = event.getMessage().getContentRaw().replace("Wordle ", "").split("\n")[0];
+                int number = Integer.parseInt(line.split(" ")[0]);
+                line = line.replace(number + "", "").trim();
+                int guesses = Integer.parseInt(line.charAt(0) + "");
+                if(guesses <= 2){
+                    event.getMessage().reply("Nice work on getting Wordle:" + number + " in "
+                    + guesses + (guesses == 1 ? " guess" : " guesses")).queue();
+                }
+            } catch (Exception ignored){
+                ignored.printStackTrace();
+            }
+        }
+    }
+}
