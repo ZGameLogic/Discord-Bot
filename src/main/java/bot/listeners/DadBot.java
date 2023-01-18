0f9b67645d239049bae09a73b5451c12de6a7ee7
Ben Shabowski <benshabowski@gmail.com>
2023-01-18 08:47:32 -0600

    Adding dad bot

--- src/main/java/bot/listeners/DadBot.java
+++ src/main/java/bot/listeners/DadBot.java
@@ -10,10 +10,17 @@ public class DadBot extends AdvancedListenerAdapter {
         if(event.getAuthor().isBot()) return;
         String message = event.getMessage().getContentRaw().toLowerCase().replaceAll("'", "");
         if(message.startsWith("im ") || message.contains(" im ")){
-            int start = message.indexOf("im") + 3;
-            System.out.println(message.substring(start));
-            String dad = message.split("im")[1].split(" ")[0];
-            event.getMessage().reply("Hi " + dad + ", I'm dad").queue();
+            String[] messageArray = message.split(" ");
+            String dad = "";
+            for(int i = 0; i < messageArray.length; i++){
+                if(messageArray[i].equals("im")){
+                    if(i < messageArray.length - 1){
+                        dad = messageArray[i+1];
+                        break;
+                    }
+                }
+            }
+            if(!dad.equals("")) event.getMessage().reply("Hi " + dad + ", I'm dad").queue();
         }
     }
 }
