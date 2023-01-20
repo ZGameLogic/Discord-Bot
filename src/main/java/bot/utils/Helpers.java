fd30764f9c77cfb060ece2c3520f1e9979706447
Ben Shabowski <benshabowski@gmail.com>
2023-01-20 12:47:04 -0600

    Including more variations

--- src/main/java/bot/utils/Helpers.java
+++ src/main/java/bot/utils/Helpers.java
@@ -10,7 +10,7 @@ import java.util.Locale;
 public abstract class Helpers {
 
     public static Date stringToDate(String dateString){
-        dateString = dateString.toUpperCase().replace("TODAY ", "").replace("AT ", "");
+        dateString = dateString.toUpperCase().replace("TODAY ", "").replace("AT ", "").replace(",", "");
         boolean tomorrow = dateString.contains("TOMORROW ");
         if(tomorrow) dateString = dateString.replace("TOMORROW ", "");
         HashMap<String, Integer[]> patterns = new HashMap<>();
@@ -18,6 +18,10 @@ public abstract class Helpers {
         patterns.put("M/dd/yyyy h:mma", new Integer[]{Calendar.YEAR, Calendar.MONTH, Calendar.DAY_OF_MONTH, Calendar.HOUR_OF_DAY, Calendar.MINUTE});
         patterns.put("M/dd h:mma", new Integer[]{Calendar.MONTH, Calendar.DAY_OF_MONTH, Calendar.HOUR_OF_DAY, Calendar.MINUTE});
         patterns.put("h:mma", new Integer[]{Calendar.HOUR_OF_DAY, Calendar.MINUTE});
+        patterns.put("EEEE h:mm a", new Integer[]{Calendar.DAY_OF_WEEK, Calendar.HOUR_OF_DAY, Calendar.MINUTE});
+        patterns.put("M/dd/yyyy h:mm a", new Integer[]{Calendar.YEAR, Calendar.MONTH, Calendar.DAY_OF_MONTH, Calendar.HOUR_OF_DAY, Calendar.MINUTE});
+        patterns.put("M/dd h:mm a", new Integer[]{Calendar.MONTH, Calendar.DAY_OF_MONTH, Calendar.HOUR_OF_DAY, Calendar.MINUTE});
+        patterns.put("h:mm a", new Integer[]{Calendar.HOUR_OF_DAY, Calendar.MINUTE});
         for(String pattern: patterns.keySet()){
             SimpleDateFormat formatter = new SimpleDateFormat(pattern, Locale.ENGLISH);
             Calendar date = Calendar.getInstance();
