05f1dcf85daac60cf36c698c6d56181efaf189b8
BShabowski <benshabowski@gmail.com>
2024-02-24 17:50:12 -0600

    Fixing this

--- src/main/java/bot/listeners/DadBot.java
+++ src/main/java/bot/listeners/DadBot.java
@@ -3,35 +3,40 @@ package bot.listeners;
 import com.zgamelogic.annotations.DiscordController;
 import com.zgamelogic.annotations.DiscordMapping;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.utils.FileUpload;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.util.Random;
 
 @DiscordController
 public class DadBot {
-
     @DiscordMapping
     public void messageReceived(MessageReceivedEvent event) {
         if(!event.isFromGuild()) return;
-        if(event.getAuthor().getIdLong() != 102923614344482816L) return;
-        String message = event.getMessage().getContentRaw().toLowerCase().replaceAll("'", "").replaceAll("’", "");
-        if(message.startsWith("im ") || message.contains(" im ")){
-            String[] messageArray = message.split(" ");
-            String dad = "";
-            boolean adding = false;
-            for(int i = 0; i < messageArray.length; i++){
-                if(adding){
-                    String word = messageArray[i];
-                    dad += word.replace(".", "") + " ";
-                    if(word.contains(".")){
-                        break;
+        if(event.getAuthor().getIdLong() == 102923614344482816L) { // Karisa's stuff
+            String message = event.getMessage().getContentRaw().toLowerCase().replaceAll("'", "").replaceAll("’", "");
+            if(message.startsWith("im ") || message.contains(" im ")){
+                String[] messageArray = message.split(" ");
+                String dad = "";
+                boolean adding = false;
+                for(int i = 0; i < messageArray.length; i++){
+                    if(adding){
+                        String word = messageArray[i];
+                        dad += word.replace(".", "") + " ";
+                        if(word.contains(".")){
+                            break;
+                        }
+                    }
+                    if(messageArray[i].equals("im")){
+                        adding = true;
                     }
                 }
-                if(messageArray[i].equals("im")){
-                    adding = true;
-                }
+                dad = dad.trim();
+                if(!dad.equals("")) event.getMessage().reply("Hi " + dad + ", I'm Dad").queue();
             }
-            dad = dad.trim();
-            if(!dad.equals("")) event.getMessage().reply("Hi " + dad + ", I'm Dad").queue();
         }
-        if(event.getAuthor().getIdLong() == 195174230281814016L){
+        if(event.getAuthor().getIdLong() == 195174230281814016L){ // Amrit's stuff
             if(event.getMessage().getContentRaw().contains("...")){
                 try {
                     int meme = new Random().nextInt(1, 16);
