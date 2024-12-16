f7520a8b61a04bc8760c35b1fc73ed09eb9cee74
Ben Shabowski <benshabowski@gmail.com>
2024-12-16 10:06:21 -0600

    Removing some not needed stuff anymore

--- src/main/java/com/zgamelogic/bot/listeners/DadBot.java
+++ src/main/java/com/zgamelogic/bot/listeners/DadBot.java
@@ -3,18 +3,9 @@ package com.zgamelogic.bot.listeners;
 import com.zgamelogic.annotations.DiscordController;
 import com.zgamelogic.annotations.DiscordMapping;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.utils.FileUpload;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.ClassPathResource;
-
-import java.io.IOException;
-import java.util.Random;
 
 @DiscordController
 public class DadBot {
-    @Value("${amrit.enabled}")
-    private boolean enabled;
-
     @DiscordMapping
     public void messageReceived(MessageReceivedEvent event) {
         if(!event.isFromGuild()) return;
@@ -37,16 +28,6 @@ public class DadBot {
                 }
                 dad = new StringBuilder(dad.toString().trim());
                 if(!dad.toString().isEmpty()) event.getMessage().reply("Hi " + dad + ", I'm Dad").queue();
-            }
-        }
-        if(enabled && event.getAuthor().getIdLong() == 195174230281814016L){ // Amrit's stuff
-            if(event.getMessage().getContentRaw().contains("...")){
-                try {
-                    int meme = new Random().nextInt(1, 16);
-                    event.getChannel().sendFiles(FileUpload.fromData(new ClassPathResource("assets/Amrit/amritmeme0" + meme + ".jpg").getFile())).queue();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
             }
         }
     }
