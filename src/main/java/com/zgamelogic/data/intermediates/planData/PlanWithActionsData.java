184284d7f79c65e2176cd4fcc6d1495caff13574
Ben Shabowski <benshabowski@gmail.com>
2025-04-19 08:31:51 -0500

    Implement custom JSON serialization for PlanWithActionsData to enhance data representation

--- src/main/java/com/zgamelogic/data/intermediates/planData/PlanWithActionsData.java
+++ src/main/java/com/zgamelogic/data/intermediates/planData/PlanWithActionsData.java
@@ -1,18 +1,24 @@
 package com.zgamelogic.data.intermediates.planData;
 
-import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.zgamelogic.data.database.planData.plan.Plan;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.List;
+import java.util.TimeZone;
 
 @Getter
 @AllArgsConstructor
+@JsonSerialize(using = PlanWithActionsData.PlanWithActionDataSerialization.class)
 public class PlanWithActionsData {
-    @JsonUnwrapped
     private Plan plan;
     private List<PlanAction> actions;
 
@@ -34,6 +40,46 @@ public class PlanWithActionsData {
 
         public static PlanAction fromButton(Button button){
             return Arrays.stream(PlanAction.values()).filter(action -> action.getId().equals(button.getId())).findFirst().orElse(null);
+        }
+    }
+
+    public static class PlanWithActionDataSerialization extends JsonSerializer<PlanWithActionsData> {
+
+        @Override
+        public void serialize(PlanWithActionsData value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+            gen.writeStartObject();
+            gen.writeNumberField("id", value.getPlan().getId());
+            gen.writeStringField("title", value.getPlan().getTitle());
+            gen.writeStringField("notes", value.getPlan().getNotes());
+            if(value.getPlan().getDate() != null) {
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+                dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+                String formattedDate = dateFormat.format(value.getPlan().getDate());
+                gen.writeStringField("start time", formattedDate);
+            }
+            gen.writeNumberField("count", value.getPlan().getCount());
+            gen.writeNumberField("author id", value.getPlan().getAuthorId());
+            gen.writeArrayFieldStart("invitees");
+            value.getPlan().getInvitees().values().forEach(planUser -> {
+                try {
+                    gen.writeStartObject();
+                    gen.writeNumberField("user id", planUser.getId().getUserId());
+                    gen.writeStringField("status", planUser.getUserStatus().name());
+                    gen.writeBooleanField("needs fill in", planUser.isNeedFillIn());
+                    gen.writeEndObject();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            gen.writeEndArray();
+            gen.writeArrayFieldStart("actions");
+            if(value.getActions() != null){
+                for(PlanAction action : value.getActions()){
+                    gen.writeString(action.name());
+                }
+            }
+            gen.writeEndArray();
+            gen.writeEndObject();
         }
     }
 }
