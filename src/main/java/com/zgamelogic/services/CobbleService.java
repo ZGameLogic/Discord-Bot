47bdc65e0b82cee443e2c8e1e3bbd8069bd7eb84
Ben Shabowski <benshabowski@gmail.com>
2025-04-23 17:05:51 -0500

    Add CobbleService and persistent storage configuration for cobble data

---
+++ src/main/java/com/zgamelogic/services/CobbleService.java
@@ -0,0 +1,30 @@
+package com.zgamelogic.services;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Service
+public class CobbleService {
+    private final File BASE_DIR;
+
+    public CobbleService() {
+        BASE_DIR = new File("cobble");
+    }
+
+    @PostConstruct
+    public void init() throws IOException {
+        File out = new File(BASE_DIR, "cobble.txt");
+        if(!out.exists()){
+            out.getParentFile().mkdirs();
+            out.createNewFile();
+        }
+        PrintWriter writer = new PrintWriter(out);
+        writer.println("Ben we made it");
+        writer.flush();
+        writer.close();
+    }
+}
