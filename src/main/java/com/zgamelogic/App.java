52caa126f28ae05459027059c45e507bf1fd2baa
Ben Shabowski <benshabowski@gmail.com>
2026-03-27 15:06:07 -0500

    Remove unnecessary comments in App.java

--- src/main/java/com/zgamelogic/App.java
+++ src/main/java/com/zgamelogic/App.java
@@ -4,9 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
- *
  * @author Ben Shabowski
- *
  */
 @SpringBootApplication
 @EnableScheduling
