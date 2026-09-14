190ecd0c6076a9f53d62f6a1efe8ab1237884738
Ben Shabowski <benshabowski@gmail.com>
2026-09-14 17:19:48 -0500

    removing headshot

--- src/main/java/com/zgamelogic/data/database/huntData/headshot/HeadShotRepository.java
+++
@@ -1,7 +0,0 @@
-package com.zgamelogic.data.database.huntData.headshot;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HeadShotRepository extends JpaRepository<HeadShot, Long> {}
